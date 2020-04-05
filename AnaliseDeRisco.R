92ac7b4378a0c7d7b7b780af60883c4a937969bf
joao souza <joaosouzaps9@gmail.com>
2020-04-05 14:38:34 -0300

    create project

---
+++ AnaliseDeRisco.R
@@ -0,0 +1,18 @@
+# Análise de Risco de Crédito
+
+# Analisando uma base de crédito histórica para conceder novo crédito 
+
+# os pacotes RandomForest - para verificar quais variaveis mais significativa para o modelo
+# O algoritmo naive bayes primeira versao e svm 2 versao
+
+setwd("C:/Users/Administrador/Desktop/DataSciecence")
+
+
+# Pacotes
+install.packages("randomForest")
+install.packages("e1071")
+library(randomForest)
+install.packages("readr")
+library(e1071)
+library(readr)
+
