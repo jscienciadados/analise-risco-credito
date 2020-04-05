b061e520dd57f999115b727babea59dddc21ab9c
joao souza <joaosouzaps9@gmail.com>
2020-04-05 14:58:12 -0300

    Normalização da base de dados

--- AnaliseDeRisco.R
+++ AnaliseDeRisco.R
@@ -1,18 +1,52 @@
-# Análise de Risco de Crédito
+# Avaliação de Risco de Crédito
 
 # Analisando uma base de crédito histórica para conceder novo crédito 
 
-# os pacotes RandomForest - para verificar quais variaveis mais significativa para o modelo
-# O algoritmo naive bayes primeira versao e svm 2 versao
-
-setwd("C:/Users/Administrador/Desktop/DataSciecence")
+# Para esta análise, vamos usar um conjunto de dados German Credit Data,
+# já devidamente limpo e organizado para a criação do modelo preditivo.
 
 
-# Pacotes
-install.packages("randomForest")
-install.packages("e1071")
+
+# Coletando os dados
+credit.df <- read.csv("credit_dataset.csv", header = TRUE, sep = ",")
+
+# Normalizando os dados
+
+# Convertendo as variáveis para o tipo fator (categoricas)
+to.factors <- function(df, variables){
+  for (variable in variables){
+    df[[variable]] <- as.factor(df[[variable]])
+  }
+  return(df)
+}
+
+# Normalização
+scale.features <- function(df, variables){
+  for (variable in variables){
+    df[[variable]] <- scale(df[[variable]], center = T, scale = T)
+  }
+  return(df)
+}
+
+# Normalizando as variáveis 
+
+# numéricas
+numeric.vars <- c("credit.duration.months", "age", "credit.amount")
+credit.df <- scale.features(credit.df, numeric.vars)
+
+# categóricas
+categorical.vars <- c('credit.rating', 'account.balance', 'previous.credit.payment.status',
+                      'credit.purpose', 'savings', 'employment.duration', 'installment.rate',
+                      'marital.status', 'guarantor', 'residence.duration', 'current.assets',
+                      'other.credits', 'apartment.type', 'bank.credits', 'occupation', 
+                      'dependents', 'telephone', 'foreign.worker')
+
+credit.df <- to.factors(df = credit.df, variables = categorical.vars)
+
+
+
+
 library(randomForest)
-install.packages("readr")
-library(e1071)
+library(caret)
 library(readr)
 
